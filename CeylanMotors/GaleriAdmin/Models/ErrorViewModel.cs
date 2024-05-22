7b375bcf7b8f7ce614d1857961e066e1ad207317
Furkan Baybura <furkan.baybura@bilgeadamboost.com>
2024-05-22 19:33:30 +0300

    +

---
+++ CeylanMotors/GaleriAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GaleriAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
